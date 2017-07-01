584ba98058fb2de3808836161cc6cf0867065870
luozhuang <1023317572@qq.com>
2017-07-01 20:00:07 +0800

    add multiplex file for select

--- server/myserver.cpp
+++ server/myserver.cpp
@@ -1,59 +1,150 @@
 #include "myserver.h"
+#include <sys/select.h>
+#include <sys/time.h>
+#include <cmath>
 
 void HttpServer::Start(int port)
 {
 
-    m_socketfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (m_socketfd == -1) {
+    m_listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (m_listenfd == -1) {
     }
 
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t clientaddr_len;
+    struct sockaddr_in server_addr;
 
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(DEFU_SERPORT);
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(m_socketfd, (struct sockaddr*)(&server_addr), sizeof(struct sockaddr_in)) == -1) {
+    if (bind(m_listenfd, (struct sockaddr*)(&server_addr), sizeof(struct sockaddr_in)) == -1) {
         //    ERROR_EXIT("bind addr failure!");
     }
 
-    if (listen(m_socketfd, DEFU_BACKLOG) == -1) {
-
+    if (listen(m_listenfd, DEFU_BACKLOG) == -1) {
+        ERROR_EXIT("bind addr failure!");
     }
 
     cout<<"server listing..."<<endl;
-    int clientfd;
 
-    if ((clientfd = accept(m_socketfd, (struct sockaddr*)(&client_addr), &clientaddr_len)) < 0) {
-        ERROR_EXIT("connect failure!");
+    //1.
+    Echo_Select();
+    //2.
+    //
+    //3.
+}
+
+void HttpServer::Echo_Select()
+{
+    struct sockaddr_in client_addr;
+    socklen_t clientaddr_len;
+
+    struct timeval tv;
+    tv.tv_sec = 5;
+    tv.tv_usec = 0;
+
+    FD_ZERO(&m_fdsetdata.readfds);
+    FD_SET(m_listenfd, &m_fdsetdata.readfds);
+
+    m_fdsetdata.client_fds.push_back(m_listenfd);
+    m_fdsetdata.maxfd_index = 0;
+
+
+    while (1) {
+        m_fdsetdata.readyfds = m_fdsetdata.readfds;
+        int ready_count = select(m_fdsetdata.client_fds[m_fdsetdata.maxfd_index] + 1, &m_fdsetdata.readyfds, NULL, NULL, &tv);
+        if (ready_count == -1) {
+            ERROR_EXIT("select fds failure!");
+        } else if (ready_count > 0) {
+            for (int i = 0; i < m_fdsetdata.client_fds.size(); ++i) {
+
+                if (m_fdsetdata.client_fds[i] == m_listenfd && FD_ISSET(m_listenfd, &m_fdsetdata.readyfds)) {
+                    int clientfd;
+                    if ((clientfd = accept(m_listenfd, (struct sockaddr*)(&client_addr), &clientaddr_len)) < 0) {
+                        ERROR_EXIT("accept client connection failure!");
+                    }
+                    cout<<clientfd<<" connect to client success!"<<endl;
+                    m_fdsetdata.client_fds.push_back(clientfd);
+                    m_fdsetdata.maxfd_index = m_fdsetdata.client_fds[m_fdsetdata.maxfd_index] > clientfd ? m_fdsetdata.maxfd_index:m_fdsetdata.client_fds.size() - 1;
+                    FD_SET(clientfd, &m_fdsetdata.readfds);
+                } else if (FD_ISSET(m_fdsetdata.client_fds[i], &m_fdsetdata.readyfds)){
+                    Echo(m_fdsetdata.client_fds[i]);
+                }
+            }
+        }
     }
+}
 
-    cout<<"connect to client success!"<<endl;
-    int ret = -1;
-    while ((ret = read(clientfd, m_buffer, BUFSIZE))) {
+void HttpServer::Echo_Poll()
+{
+
+}
+
+void HttpServer::Echo_Epoll()
+{
+
+}
+
+void HttpServer::Echo(int clientfd)
+{
+    memset(m_buffer, 0, BUFSIZE);
+
+    int ret;
+    while ((ret = read(clientfd, m_buffer, BUFSIZE - 1))) {
         if (ret != -1) {
             if (ret == 0) {
                 cout<<"client disconnect!"<<endl;
+                FD_CLR(clientfd, &m_fdsetdata.readfds);
+                RemoveValue(m_fdsetdata.maxfd_index, m_fdsetdata.client_fds);
+                close(clientfd);
                 break;
             } else {
                 cout<<m_buffer;
                 ret = write(clientfd, m_buffer, BUFSIZE);
-                memset(m_buffer, 0 , BUFSIZE);
                 if (ret == -1) {
                     ERROR_EXIT("write data section to client error!");
                 }
+                memset(m_buffer, 0 , BUFSIZE);
             }
-
         }
         else {
             ERROR_EXIT("read data section from client error!");
         }
     }
-
-    close(clientfd);
-    cout<<"Client disconnect!"<<endl;
 }
 
+void  HttpServer::RemoveValue(int& index, vector<int> & fds)
+{
+    int first = -1, second = -1;
+    for (int i = 0; i < fds.size(); ++i) {
+        if (first == -1) {
+            first = i;
+            continue;
+        }
+
+        if (second == -1) {
+            if (fds[first] < fds[i]) {
+                first = i;
+                second = first;
+            } else {
+                second = i;
+            }
+            continue;
+        }
+
+        if (fds[i] >= fds[first]) {
+            second = first;
+            first = i;
+        } else if (fds[i] > fds[second]) {
+            second = i;
+        }
+    }
+
+    if (first != -1) fds.erase(fds.begin() + first);
+    if (second != -1) {
+        if (second < first)index = second;
+        else index = second - 1;
+    }
+
+}
 
 
--- server/myserver.h
+++ server/myserver.h
@@ -5,6 +5,7 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <iostream>
+#include <vector>
 #include <cstdlib>
 #include <cstdio>
 #include <cstring>
@@ -31,8 +32,26 @@ public:
 
     }
     void Start(int port = DEFU_SERPORT);
+
+    //three way for echo implementing
+    void Echo_Select();
+    void Echo_Poll();
+    void Echo_Epoll();
+    void Echo(int client);
+    void RemoveValue(int& index, vector<int>& client);
 private:
-    int m_socketfd;
+    int m_listenfd;
     char m_buffer[BUFSIZE];
+
+    //select for echo implement
+    //fd_set m_readfds;
+    struct SelectData
+    {
+        vector<int> client_fds;
+        int maxfd_index;
+        fd_set  readfds, readyfds;
+    };
+
+    SelectData m_fdsetdata;
 };
 #endif
